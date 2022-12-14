0e25350fd04d0a792d4f31414d38cf79c9dd1dca
prestonator <preston.b.jenkins-1@ou.edu>
2022-12-14 12:03:06 -0600

    added layout component with styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,19 @@ module.exports = {
     title: `My First Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        icon: `src/images/icon.png`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
 }
